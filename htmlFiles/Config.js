a1228546453821f47e8305245ca591d189589a5f
ArwaVasaiwala <arwa.vasaiwala@gmail.com>
2025-10-26 10:40:46 +0530

    Add Firebase configuration and initialization
    This file contains the Firebase configuration and initialization code for the web app.

---
+++ htmlFiles/Config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCmYfkIFvqYCzKkJnuWD_ypqqLTgw78tLU",
+    authDomain: "project-palette-planner.firebaseapp.com",
+    projectId: "project-palette-planner",
+    storageBucket: "project-palette-planner.firebasestorage.app",
+    messagingSenderId: "106139640454",
+    appId: "1:106139640454:web:37da1fc10231f8e7492707"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
